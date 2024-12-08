25e277cac93feee3fb1fa91502791fd4cafe109a
cosmin <cosmin_popovici2012@yahoo.com>
2024-12-08 10:45:12 +0200

    Fixed user profile view page

--- running_app/lib/home/home_view_page.dart
+++ running_app/lib/home/home_view_page.dart
@@ -102,7 +102,7 @@ class _HomeViewPageState extends State<HomeViewPage> {
                 const TourRecordPage(),
                 InternetConnectionChecker(
                     child: UserProfileViewPage(
-                  isEditable: false,
+                  isEditable: true,
                 ))
               ],
             ),
